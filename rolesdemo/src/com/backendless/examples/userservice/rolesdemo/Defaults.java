e78628f84d9a41f8c5f01b498158b55b70717201
mark <mark@themidnightcoders.com>
2013-07-13 15:57:31 -0500

    initial commit

---
+++ rolesdemo/src/com/backendless/examples/userservice/rolesdemo/Defaults.java
@@ -0,0 +1,14 @@
+package com.backendless.examples.userservice.rolesdemo;
+
+public class Defaults
+{
+  public static final String IDENTITY = "name";
+  public static final String ROLE_TAG = "role";
+
+  public static final String READ_WRITE_ROLE = "ReadWriteRole";
+  public static final String READ_ROLE = "ReadOnlyRole";
+
+  public static final String APP_ID = "PUT-YOUR-APP-ID-HERE";
+  public static final String SECRET_KEY = "PUT-YOUR-SECRET-KEY-HERE";
+  public static final String VERSION = "v1";
+}
